d2835cab9883e98487b51fca94a7ae55a4976ed1
yigithanbalci <yigithanbalci@gmail.com>
2019-12-19 22:00:34 +0300

    configurations have been done

---
+++ src/main/java/com/dingiller/halisahasepeti/HalisahasepetiApplication.java
@@ -0,0 +1,13 @@
+package com.dingiller.halisahasepeti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalisahasepetiApplication {
+
+	public static void main (String[] args) {
+		SpringApplication.run(HalisahasepetiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dingiller/halisahasepeti/HalisahasepetiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dingiller.halisahasepeti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalisahasepetiApplicationTests {
+
+	@Test
+	void contextLoads () {
+	}
+
+}
