968f9d29c4c9c8ca0d792e54121ba1fadf89dc13
yigithanbalci <yigithanbalci@gmail.com>
2019-12-22 22:18:22 +0300

    Revert "second commit"
    This reverts commit 94ee254352d7055da4497551ac698c7b4ffe582e.

--- src/main/java/com/dingiller/halisahasepeti/HalisahasepetiApplication.java
+++ src/main/java/com/dingiller/halisahasepeti/HalisahasepetiApplication.java
@@ -1,13 +1,10 @@
 package com.dingiller.halisahasepeti;
 
-import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HalisahasepetiApplication {
-
-	private static Logger log = Logger.getLogger(HalisahasepetiApplication.class);
 
 	public static void main (String[] args) {
 		SpringApplication.run(HalisahasepetiApplication.class, args);
